f4a0c795f63a65b3304c5e3f0d735017dae9b7cb
Ethan Perelmuter <peethan@sas.upenn.edu>
2019-11-22 19:49:06 -0500

    Derived (Lat, Long, Biomass (Mg/Hectare)) table from Puerto_Rico_Biomass.img

--- DatasetsTesting/puerto_rico_data.py
+++ DatasetsTesting/puerto_rico_data.py
@@ -1,10 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import rasterio
 import rasterio.features
 import rasterio.warp
 
-img_name = "Puerto_Rico_Biomass.img"
+# ****Uncomment this next section see the dataset ****
+# df = pd.read_csv('PR_Biomass_Coordinates_Dataset.csv', index_col=False)
+# df_biomass_exists = df[df['biomass'] > 0]
+# display(df_biomass_exists)
+# t1 = len(df)
+# t2 = len(df_biomass_exists)
+# pct = t2 / t1
+# pct = (pct * 100)
+# print(str(pct) + "% of the map contains biomass")
+
+
+
+img_name = "DatasetsTesting/Puerto_Rico_Biomass.img"
 output_filename = 'image1.png'
 
 def print_stats(ds):
@@ -22,65 +35,43 @@ def print_stats(ds):
     {i: dtype for i, dtype in zip(dataset.indexes, dataset.dtypes)}
 
 dataset = rasterio.open(img_name)
-print_stats(dataset)
+height = dataset.height
+width = dataset.width
+# print_stats(dataset)
 
 band1 = dataset.read(1)
-shape = (349, 929)
-print(band1.shape)
-image = band1.reshape(shape)
-
 # Display.
-plt.imshow(image, cmap = "gray")
+plt.imshow(band1, cmap = "gray")
 plt.savefig(output_filename)
 plt.show()
 
+# Goal: Build table of (latitude, longitude, pixel value) tuples
 
+data = band1
+max_lat = 18.5542 # North
+min_lat = 17.7694 # South
 
+max_long = -65.13 # East Border
+min_long = -67.3228 # West Border
+diff_x = max_long - min_long
+print(diff_x/width)
+longitude_delta = diff_x/929
 
+diff_y = max_lat - min_lat
+print(diff_y/height)
+latitude_delta = diff_y/349
+lat = 18.5542
+long = -67.3228
+lat_long_data = []
+for x in range(0, 929):
+    lat = 18.5542 # Set longitude to far North (Top)
+    for y in range(0, 349):
+        bm = data[y, x]
+        # coord_biomass = coord_biomass.append({'latitude': lat, 'longitude': long, 'biomass':bm}, ignore_index=True)
+        lat_long_data.append([lat, long, bm])
+        lat = lat - latitude_delta
+    long = long + longitude_delta
+coord_biomass = pd.DataFrame(data=lat_long_data, columns=['latitude', 'longitude', 'biomass'])
+coord_biomass.to_csv('PR_Biomass_Coordinates_Dataset.csv', index=False)
 
-
-# Read the dataset's valid data mask as a ndarray.
-mask = dataset.dataset_mask()
-
-# Extract feature shapes and values from the array.
-for geom, val in rasterio.features.shapes(
-        mask, transform=dataset.transform):
-
-    # Transform shapes from the dataset's own coordinate
-    # reference system to CRS84 (EPSG:4326).
-    geom = rasterio.warp.transform_geom(
-        dataset.crs, 'EPSG:4326', geom, precision=6)
-
-    # Print GeoJSON shapes to stdout.
-    print(geom)
-
-fp = "Puerto_Rico_Biomass.img"
-raster = rasterio.open(fp)
-
-type(raster)
-
-
-
-
-
-
-for i in range(1, 502960):
-    if 502960 % i == 0:
-        print("Coordinates: " + str(502960/i) + " by " + str(i))
-
-# Parameters.
-input_filename = "Puerto_Rico_Biomass.img"
-shape = (6287, 80) # matrix size
-dtype = np.dtype(np.int8) # big-endian unsigned integer (16bit)
-output_filename = "TestImage.PNG"
-
-# Reading.
-fid = open(input_filename, 'rb')
-data = np.fromfile(fid, dtype)
-image = data.reshape(shape)
-# image = data
-
-# Display.
-plt.imshow(image, cmap = "gray")
-plt.savefig(output_filename)
-plt.show()
+display(coord_biomass[coord_biomass['biomass'] > 0])

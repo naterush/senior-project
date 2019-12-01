7ebe2b40eb3e32734751f88f3534f11af6cdb27a
naterush <naterush1997@gmail.com>
2019-12-01 16:10:39 -0500

    add todos

--- DatasetsTesting/data_download.py
+++ DatasetsTesting/data_download.py
@@ -26,7 +26,19 @@ def main():
     """
     for index, row in coord_biomass.iterrows():
         api.download(row['latitude'], row['longitude'])
+        # TODO: 
+        # 1. have api.download return a path to a zip file that was downloaded
+        # (and maybe some other meta-data like size, or covered region or something)
+        # and then we down redownload data we already have
+        # 2. write a "pre-process" function that takes this path and 
+        #   a. opens the zip file, gets the specific image we are interested in
+        #   b. reads in image, and pre-processes it how we see fit
+        #   c. deletes the rest of the data and saves the final result
+        # This will reduce the size of the data we are storing at any point in time
+        # (which I think could get pretty large if we aren't careful...)
     """
+
+    # TODO: feed this shit into a model???? idk
     
 
 

ac843d3ba8cbfc9e62d7737b0af2e59c14c6da4b
Maria <maturner@seas.upenn.edu>
2020-04-07 11:56:56 -0400

    function to parse coordinate inputs

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+//var client = require('./client')
 var app = express();
 
 app.engine('html', require('ejs').__express);
@@ -7,44 +8,70 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 
-dummyjsonFile = {
-  "glossary": {
-      "title": "example glossary",
-  "GlossDiv": {
-          "title": "S",
-    "GlossList": {
-              "GlossEntry": {
-                  "ID": "SGML",
-        "SortAs": "SGML",
-        "GlossTerm": "Standard Generalized Markup Language",
-        "Acronym": "SGML",
-        "Abbrev": "ISO 8879:1986",
-        "GlossDef": {
-                      "para": "A meta-markup language, used to create markup languages such as DocBook.",
-          "GlossSeeAlso": ["GML", "XML"]
-                  },
-        "GlossSee": "markup"
-              }
-          }
+
+function getJSONData(latitude, longitude, radius) {
+  // Within a radius of this point, return the heat map results
+  //var coords = JSON.parse(dummyjsonMap);
+  var coords = dummyMap.coordinates;
+  var selected = [];
+  for (c in coords) {
+    if (c.latitude < latitude + radius && 
+      c.latitude > latitude - radius &&
+      c.longitude < longitude + radius &&
+      c.longitude > longitude - radius) {
+        selected.push(c);
       }
   }
-};
 
-dummyjsonMap = {
-  "coordinates" : [
-    {"latitude": "40.416775",
-     "longitude": "-3.70379",
-      "color": "GREEN"}, 
-
-    {"latitude": "55.378051",
-    "longitude": "-3.435973",
-    "color": "RED"}]
-};
-
-
-function getJSONData(point, radius) {
-  return dummyjsonFile;
+  return selected;
 }
+
+
+// TODO: this should be a JSON object input not a regular object
+var dummyMap =
+{
+	"coordinates": [
+		{
+			"latitude": "40.416775",
+			"longitude": "-3.70379",
+			"color": "GREEN",
+			"weight": "6"
+		},
+		{
+			"latitude": "41.385064",
+			"longitude": "2.173403",
+			"color": "GREEN",
+			"weight": "2"
+		},
+		{
+			"latitude": "52.130661",
+			"longitude": "-3.783712",
+			"color": "GREEN",
+			"weight": "2"
+		},
+		{
+			"latitude": "55.378051",
+			"longitude": "-3.435973",
+			"color": "GREEN",
+			"weight": "8"
+		},
+		{
+			"latitude": "-40.900557",
+			"longitude": "-174.885971",
+			"color": "GREEN",
+			"weight": "6"
+		},
+		{
+			"latitude": "40.714353",
+			"longitude": "-74.005973",
+			"color": "RED",
+			"weight": "6"
+		}
+	]
+};
+
+
+
 
 app.get('/', function (req, res) {
   res.render('map.html');
@@ -54,12 +81,43 @@ app.get('/sapling', function (req, res) {
   res.render('sapling.ejs.html');
 });
 
+
 app.get('/getRegion', function (req, res) {
+  //client.getRegionFunction();
+  console.log("Reached");
   //Take point, radius. Return "map overlay data"
-  point = req.param("POINT");
-  radius = req.param("RADIUS");
+  //latitude = req.param("LATITUDE");
+  //longitude = req.param("LONGITUDE");
+ // radius = req.param("RADIUS");
+ latitude = 20;
+ longitude = 100;
+ radius = 500;
   // Call some function here on point and radius to return JSON data
-  res.json(getJSONData(point, radius));
+  //jsonData = getJSONData(latitude, longitude, radius);
+  var coords = [];
+  var leftbottomlat = latitude - radius
+  var leftbottomlong = longitude - radius;
+  var rightbottomlat = latitude + radius;
+  var rightbottomlong = latitude - radius;
+  var lefttoplat = latitude - radius;
+  var lefttoplong = latitude + radius;
+  var righttoplat = latitude + radius;
+  var righttoplong = longitude + radius;
+  res.render('map.html');
+  /*for (d in jsonData) {
+    coords += { location: new google.maps.LatLng(d.latitude, d.longitude), weight: d.weight }
+  }*/
+
+  /*document.getElementById("leftbottomlat").innerHTML = leftbottomlat;
+  document.getElementById("leftbottomlong").innerHTML = leftbottomlong;
+  document.getElementById("rightbottomlat").innerHTML = rightbottomlat;
+  document.getElementById("rightbottomlong").innerHTML = rightbottomlong;
+  document.getElementById("lefttoplat").innerHTML = lefttoplat;
+  document.getElementById("lefttoplong").innerHTML = lefttoplong;
+  document.getElementById("righttoplat").innerHTML = righttoplat;
+  document.getElementById("righttoplong").innerHTML = righttoplong;*/
+  //document.getElementById("coords").innerHTML = coords;
+  //res.json(getJSONData(latitude, longitude, radius));
 });
 
 app.listen(3000);

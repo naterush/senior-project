7e1124b1f0a545eeebefa7db5979d3dc002bb8a0
Ethan Perelmuter <peethan@sas.upenn.edu>
2020-04-16 15:52:34 -0400

    More work on RGB

--- model/data-pipeline/merge_bands.py
+++ model/data-pipeline/merge_bands.py
@@ -4,23 +4,22 @@ import matplotlib.pyplot as plt
 import earthpy
 import earthpy.spatial as es
 import earthpy.plot as ep
+import rasterio as rio
 
 
 all_landsat_post_bands = []
-for i in range(1, 4):
+for i in range(1, 5):
     s = 'model/data-pipeline/downloaded_sat_data/ethan_test_LC08/LC08_L1TP_014032_20200316_20200326_01_T1_B'
     s = s + str(i)+'.TIF'
     all_landsat_post_bands.append(s)
 all_landsat_post_bands
+output_fp = 'model/data-pipeline/downloaded_sat_data/ethan_test_LC08/a.tif'
+(stack, meta) = es.stack(all_landsat_post_bands, output_fp)
 
-(stack, meta) = es.stack(all_landsat_post_bands, 'model/data-pipeline/downloaded_sat_data/ethan_test_LC08/a.tif')
-print(type(meta))
-
-# all_landsat_post_bands.sort()
-# Create an output array of all the landsat data stacked
-landsat_post_fire_path = os.path.join("data", "cold-springs-fire",
-                                      "outputs", "landsat_post_fire.tif")
-
-# This will create a new stacked raster with all bands
-land_stack, land_meta = es.stack(all_landsat_post_bands,
-                                 landsat_post_fire_path)
+with rio.open(output_fp) as src:
+    landsat_post_fire = src.read()
+    ep.plot_rgb(landsat_post_fire,
+            rgb=[3, 2, 1],
+            title="RGB Composite Image\n Post Fire Landsat Data",
+            stretch = True)
+    plt.show()
